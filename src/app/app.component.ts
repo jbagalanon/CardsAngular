a024126601463a5e09a5dbd840ed32a084e2c309
Joemar <jbagalanon@gmail.com>
2020-08-22 22:03:24 +0800

    Change manual to looping and add css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,10 +20,22 @@ export class AppComponent {
       content: 'A snowy mountain in the west',
     },
     {
-      title: 'Neat Tree',
+      title: 'Bikings',
       imageUrl: 'assets/biking.jpeg',
       username: 'cycling',
-      content: 'I dic some exercise today',
+      content: 'I did some exercise today',
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this tree while vacationing',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountailover',
+      content: 'A snowy mountain in the west',
     },
   ];
 }
